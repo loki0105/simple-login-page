1dd12c7758c87090739d2b846366e19a3e3ccbb5
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-01-30 02:15:26 +0900

    added register and login using postgres database

--- index.js
+++ index.js
@@ -33,12 +33,32 @@ const db = new pg.Client({
 db.connect();
 
 app.get("/", async (req, res) => {
-	const result = await db.query("select * from users");
-	console.log(result.rows);
-	res.render("index.ejs", { loginFound: true });
+
+	res.render("index.ejs");
 });
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	
+
+try {
+	const result = await db.query("select * from users where username =$1",
+	[email]);
+	if (result.rows.length < 0) {
+		res.redirect("no username");
+	} else {
+		console.log(result.rows[0].password);
+			console.log(result.rows[0].username);
+		if (result.rows[0].password == password) {
+			res.render("congrats.ejs");
+		}
+	}
+} catch (error) {
+	console.log(error);
+}
+});
 
 app.get("/regpage", (req, res) => {
 	res.render("reg.ejs");
@@ -46,8 +66,8 @@ app.get("/regpage", (req, res) => {
 
 //newemail newpassword newpascheck
 
-app.get("/register",async (req, res) => {
-	const email = req.body.form2Example1;
+app.post("/register",async (req, res) => {
+	const email = req.body.newemail; 
 	const password = req.body.newpassword;
 	const checkupp = req.body.newpascheck;
 	console.log(email, password);
@@ -59,14 +79,15 @@ app.get("/register",async (req, res) => {
 			if (result.rows.length !== 0)
 			{
 				console.log("email is used");
-				req.redirect("/login");
+				req.redirect("/");
 			} else {
 				const cresult = await db.query(
 					"insert into users (username, password) values ($1, $2) returning *",
-					[email, passport]
+					[email, password]
 				);
 				
 				console.log(cresult.rows);
+				res.render("congrats.ejs");
 				
 			}
 			
