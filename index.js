4d59be13166ef894ddbfa50217c7f3254926bd81
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-01-30 01:53:18 +0900

    working on connecting database

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ import { Strategy } from "passport-local";
 import session from "express-session";
 import env from "dotenv";
 
-
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
 
-
-
-
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -21,40 +26,63 @@ app.use(express.static("public"));
 const db = new pg.Client({
 	user: process.env.PG_USER,
 	host: process.env.PG_HOST,
-	database: process.env.PG_DATABASE,
+	database: process.env.PG_NAME,
 	password: process.env.PG_PASSWORD,
 	port: process.env.PG_PORT,
 });
 db.connect();
 
-
-
 app.get("/", async (req, res) => {
-const result = await db.query("select * from users");
-console.log(result.rows);
+	const result = await db.query("select * from users");
+	console.log(result.rows);
 	res.render("index.ejs", { loginFound: true });
 });
 
+app.post("/submit", (req, res) => {});
 
-app.post("/submit", (req, res) => {
-
+app.get("/regpage", (req, res) => {
+	res.render("reg.ejs");
 });
 
-app.post("/check", (req, res) => {
+//newemail newpassword newpascheck
 
-});
+app.get("/register",async (req, res) => {
+	const email = req.body.form2Example1;
+	const password = req.body.newpassword;
+	const checkupp = req.body.newpascheck;
+	console.log(email, password);
+	if (password == checkupp) {
+
+		const result = await db.query("select  * from users where username = $1", [email]);
+
+		try {
+			if (result.rows.length !== 0)
+			{
+				console.log("email is used");
+				req.redirect("/login");
+			} else {
+				const cresult = await db.query(
+					"insert into users (username, password) values ($1, $2) returning *",
+					[email, passport]
+				);
+				
+				console.log(cresult.rows);
+				
+			}
+			
+		} catch (error) {
+			console.log(error)
+			
+		}
 
 
-
-app.get("/register", (req, res) => {
+ 
+	}   
+ 
 	
 });
 
-app.get("/tryagain", (req, res) => {
-	
-});
-
-
+app.get("/tryagain", (req, res) => {});
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
