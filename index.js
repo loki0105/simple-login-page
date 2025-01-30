9ff34acaa31d83ee59ce2dbe20334f1c5af2fa7b
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-01-30 18:24:35 +0900

    added bcrypt for registering and loging

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ const db = new pg.Client({
 db.connect();
 
 app.get("/", async (req, res) => {
-
 	res.render("index.ejs");
 });
 
@@ -41,24 +40,37 @@ app.post("/submit", async (req, res) => {
 	const email = req.body.email;
 	const password = req.body.password;
 	console.log("login page", email, password);
-	
-
-try {
-	const result = await db.query("select * from users where username =$1",
-	[email]);
-	if (result.rows.length == 0) {
-		res.redirect("no username");
-	} else {
-		console.log(result.rows[0].password);
-		console.log(result.rows[0].username);
-		if (result.rows[0].password == password) {
-			res.render("congrats.ejs");
+	try {
+		const result = await db.query("select * from users where username =$1", [
+			email,
+		]);
+		if (result.rows.length == 0) {
+			res.redirect("no username");
+		} else {
+			const hash = result.rows[0].password;
+			bcrypt.compare(password, hash, function (err, result) {
+				if (err) {
+					console.log(err)
+					
+				} else {
+					if (result) {
+						res.render("congrats.ejs")
+						
+					} else
+					{
+						res.render("/");
+						}
+					
+				}
+				
+			});
 		}
+	} catch (error) {
+		console.log(error);
 	}
-} catch (error) {
-	console.log(error);
-}
 });
+
+
 
 app.get("/regpage", (req, res) => {
 	res.render("reg.ejs");
@@ -66,41 +78,38 @@ app.get("/regpage", (req, res) => {
 
 //newemail newpassword newpascheck
 
-app.post("/register",async (req, res) => {
-	const email = req.body.newemail; 
+app.post("/register", async (req, res) => {
+	const email = req.body.newemail;
 	const password = req.body.newpassword;
 	const checkupp = req.body.newpascheck;
 	console.log(email, password);
 	if (password == checkupp) {
-
-		const result = await db.query("select  * from users where username = $1", [email]);
-
 		try {
-			if (result.rows.length !== 0)
-			{
+			const result = await db.query(
+				"select  * from users where username = $1",
+				[email]
+			);
+			if (result.rows.length !== 0) {
 				console.log("email is used");
 				req.redirect("/");
 			} else {
-				const cresult = await db.query(
-					"insert into users (username, password) values ($1, $2) returning *",
-					[email, password]
-				);
-				
-				console.log(cresult.rows);
-				res.render("congrats.ejs");
-				
+				bcrypt.hash(password, saltRounds, async function (err, hash) {
+					if (err) {
+						console.log(err);
+					} else {
+						const cresult = await db.query(
+							"insert into users (username, password) values ($1, $2) returning *",
+							[email, hash]
+						);
+						console.log(cresult.rows);
+						res.render("congrats.ejs");
+					}
+				});
 			}
-			
 		} catch (error) {
-			console.log(error)
-			
+			console.log(error);
 		}
-
-
- 
-	}   
- 
-	
+	}
 });
 
 app.get("/tryagain", (req, res) => {});
